1d4806816808a39afa997e302a7b4aff4f4afddc
AdykankovVS <105042656+AdykankovVS@users.noreply.github.com>
2022-06-03 06:15:08 +0300

    Add files via upload

---
+++ Тема4/Лаб5/Задание1/t4l5z1/t4l5z1.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, m, n, i;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "d = "; cin >> d;
+
+	m = a * d + b * c;
+	n = b * d;
+	i = 2;
+
+	while (i <= n && i <= m)
+	{
+		if (n % i == 0 && m % i == 0)
+		{
+			n = n / 1;
+			m = m / 1;
+		}
+
+		else i++;
+	}
+		cout << m << "/" << n;
+		return 0;
+}
---
+++ Тема4/Лаб5/Задание2/t4l5z2/t4l5z2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <clocale>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian_Russia.1251");
+
+    using namespace std;
+
+    int n, nNumSum = 0, temp, iNumSum = 0, printedNumCount = 0;
+    cout << "Введите число n = "; cin >> n;
+
+    temp = n;
+
+    while (temp > 0)
+    {
+        nNumSum += temp % 10;
+        temp /= 10;
+    }
+
+    for (int i = 1; i < n; i++) {
+        temp = i;
+
+        while (temp > 0)
+        {
+            iNumSum += temp % 10;
+            temp /= 10;
+        }
+
+        if (iNumSum == nNumSum) {
+            cout << i << endl;
+            printedNumCount++;
+        }
+
+        iNumSum = 0;
+
+    }
+
+    if (printedNumCount == 0) cout << "Нет искомых чисел!";
+}
