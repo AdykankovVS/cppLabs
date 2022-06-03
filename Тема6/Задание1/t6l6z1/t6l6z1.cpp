5b339f721d308f1af70dd96b98270ad5bda1586c
AdykankovVS <105042656+AdykankovVS@users.noreply.github.com>
2022-06-03 06:15:54 +0300

    Add files via upload

---
+++ Тема6/Задание1/t6l6z1/t6l6z1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+void vvod(int a[], int n);
+
+void ravn(int n, int a[], int k, int l);
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian_Russia.1251");
+    int k = -1;
+    int l = 0;
+    int a[20];
+    int n;
+
+    cout << "Введите колличество элементов массива - "; cin >> n;
+
+    vvod(a, n);
+
+    ravn(n, a, k, l);
+
+    if (k == -1)
+        //cout << "k = " << k << " l = " << l;
+        cout << "Равных элементов нет" << endl;
+    else
+        cout << "k = " << k << " l = " << l;
+    //cout << "Равных элемнтов нет" << endl;
+}
+
+void ravn(int n, int a[], int k, int l)
+{
+
+    for (int i = 0; i <= (n - 2); i++)
+    {
+        for (int j = (i + 1); j <= (n - 1); j++)
+        {
+            if (a[i] = a[j])
+            {
+                k = i;
+                l = j;
+            }
+            else { continue; }
+        }
+    }
+}
+
+void vvod(int a[], int n)
+{
+    cout << "Введите элементы целочисленного массива: " << endl;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+}
---
+++ Тема6/Задание2/t6l6z2/t6l6z2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+int  array_max(const int* a, const int* b);
+int* array_remove(int* a, int* b, int v);
+
+using namespace std;
+
+int main(void) {
+
+    setlocale(LC_ALL, "Russian_Russia.1251");
+    const int n = 10;
+    cout << "Ввод [" << n << ']' << std::endl;
+
+    int a[n];
+    for (int i = 0; i < n; ++i) {
+        cout << '[' << i << "] = ";
+        cin >> a[i];
+    }
+    cin.sync();
+
+    int* e = array_remove(a, a + n, array_max(a, a + n));
+    for (int* p = a; p != e; ++p)
+        cout << *p << ' ';
+        cin.get();
+
+    return 0;
+}
+
+int array_max(const int* a, const int* b) {
+    const int* p = a;
+    for (++a; a < b; ++a) {
+        if (*a > *p)
+            p = a;
+    }
+    return (p != b) ? *p : 0;
+}
+
+int* array_remove(int* a, int* b, int v) {
+    while ((a < b) && (*a != v))
+        ++a;
+
+    int* p = a;
+    for (; a < b; ++a) {
+        if (*a != v)
+            *p++ = *a;
+    }
+    return p;
+}
