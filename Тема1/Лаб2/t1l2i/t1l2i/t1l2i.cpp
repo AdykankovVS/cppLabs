4f8239663adeec7844c0f49323913e2445e9195d
AdykankovVS <105042656+AdykankovVS@users.noreply.github.com>
2022-06-03 06:12:36 +0300

    Add files via upload

---
+++ Тема1/Лаб2/t1l2i/t1l2i/t1l2i.cpp
@@ -0,0 +1,28 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    double u, x, y, w, v, f, z;
+
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+    cout << "z = "; cin >> z;
+
+    f = (-1) * exp(x * (sqrt(fabs(y)))) + z;
+
+    v = sqrt(fabs((sin(cos(f)), 2) + x)), 3;
+
+    w = f + v + log(fabs(x * y * z));
+
+    u = x + y + exp((-1) * (w * v * f));
+
+    cout << "U = " << u << endl;
+
+    return 0;
+}
